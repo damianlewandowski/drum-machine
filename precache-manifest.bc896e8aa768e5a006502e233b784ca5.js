61f2471b331b76818ee24a61976f6829ce3020a2
Damian <damian20lewandowski@gmail.com>
2019-04-12 09:29:09 +0200

    Updates

---
+++ precache-manifest.bc896e8aa768e5a006502e233b784ca5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ace694fde820c8225a0c",
+    "url": "/drum-machine/static/js/main.e7fb7c7c.chunk.js"
+  },
+  {
+    "revision": "c73e5ac2cc3e61c10a29",
+    "url": "/drum-machine/static/js/runtime~main.b16cf6cd.js"
+  },
+  {
+    "revision": "216770fc4ab194463d7a",
+    "url": "/drum-machine/static/js/2.4df69278.chunk.js"
+  },
+  {
+    "revision": "7fc5a695417d02d446af62f5b7166612",
+    "url": "/drum-machine/static/media/crash.7fc5a695.wav"
+  },
+  {
+    "revision": "e35f87bba02646a9660cd0c6b046c003",
+    "url": "/drum-machine/static/media/crash2.e35f87bb.wav"
+  },
+  {
+    "revision": "647a17c03567d686dc7eb472139eaf49",
+    "url": "/drum-machine/static/media/hat.647a17c0.wav"
+  },
+  {
+    "revision": "152e8f12ea8f4974bc1c56762e03f1da",
+    "url": "/drum-machine/static/media/kick.152e8f12.wav"
+  },
+  {
+    "revision": "f29438012cc6787d87a865c504fd0479",
+    "url": "/drum-machine/static/media/revcrash.f2943801.wav"
+  },
+  {
+    "revision": "49f8d781b8b05824bf841e3a41771043",
+    "url": "/drum-machine/static/media/ride.49f8d781.wav"
+  },
+  {
+    "revision": "e8c95ef2e6d9323ba9467730b9c609b9",
+    "url": "/drum-machine/static/media/sdst.e8c95ef2.wav"
+  },
+  {
+    "revision": "8c60cff1ad7eb4e534143d19bde95b8f",
+    "url": "/drum-machine/static/media/snr.8c60cff1.wav"
+  },
+  {
+    "revision": "35a0299946dcd116ad480fcc0baa07b4",
+    "url": "/drum-machine/static/media/tom.35a02999.wav"
+  },
+  {
+    "revision": "4bc210c3c7c7e0c96e5582a24cd5e7bb",
+    "url": "/drum-machine/index.html"
+  }
+];
